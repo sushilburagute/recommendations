91489b3ea0503c7fd7f8c34a30a3eb27627ca582
Sushil Buragute <50674860+sushilburagute@users.noreply.github.com>
2020-12-08 01:17:16 +0530

    Initital Commit create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>🥇Recommendations by Sush</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
