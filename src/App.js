4f27f00a7ef9dff908297991a6d9445df731254b
Sushil Buragute <50674860+sushilburagute@users.noreply.github.com>
2020-12-08 06:24:28 +0530

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { useState } from "react";
 import "./App.css";
+import { recomDB } from "./recomDB";
 
 function App() {
+  const [categorySelected, setcategorySelected] = useState("food");
+
   return (
     <div className="App">
-      <div>
-        <h1>🥇Recommendations by Sush</h1>
+      <div className="main">
+        <div className="card">
+          <h2>🥇Recommendations by Sush</h2>
+        </div>
+
+        <div className="card">
+          <div>
+            {Object.keys(recomDB).map((category) => (
+              <button onClick={() => setcategorySelected(category)}>
+                {category}
+              </button>
+            ))}
+          </div>
+
+          <div className="lists">
+            <ul>
+              {recomDB[categorySelected].map((item) => (
+                <li>
+                  <div className="list-heading">{item.name}</div>
+                  <div className="list-rating">{item.rating}</div>
+                  <div className="list-descrip">{item.descrip}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/recomDB.js
@@ -0,0 +1,64 @@
+export const recomDB = {
+  books: [
+    {
+      name: "A Promised Land",
+      rating: "5⭐",
+      descrip:
+        "A riveting, deeply personal account of history in the making—from the president who inspired us to believe in the power of democracy",
+    },
+    {
+      name: "Haunted Empire: Apple After Steve Jobs",
+      rating: "4.4⭐",
+      descrip:
+        "Yukari Iwatani Kane delves deep inside Apple in the two years since Steve Jobs’s death, revealing the tensions and challenges CEO Tim Cook and his team face as they try to sustain Jobs’s vision and keep the company moving forward.",
+    },
+    {
+      name: "The Amazon Management System",
+      rating: "4⭐",
+      descrip:
+        "Amazon's trillion-dollar success is the envy of everyone, but achievable by anyone. What has propelled their record streak of growth? Their management system.",
+    },
+  ],
+
+  movies: [
+    {
+      name: "The Judge",
+      rating: "4.7⭐",
+      descrip:
+        "The Judge is a timeless film, in that it could have been made at almost any point over the past 80 years: rote plot, functional support, well-signalled twists. It's a two-seater star vehicle offering little legroom for other passengers.",
+    },
+    {
+      name: "The Social Dilemma",
+      rating: "4.4⭐",
+      descrip:
+        "Never before have a handful of tech designers had such control over the way billions of us think, act, and live our lives. Do I need to exaplain more?",
+    },
+    {
+      name: "Bohemian Rhapsody",
+      rating: "4.2⭐",
+      descrip:
+        "With his impeccable vocal abilities, Freddie Mercury and his rock band, Queen, achieve superstardom. However, amidst his skyrocketing success, he grapples with his ego, sexuality and a fatal illness.",
+    },
+  ],
+
+  food: [
+    {
+      name: "Vada Pav",
+      rating: "5⭐",
+      descrip:
+        "Vada pav or wada pav is the common man’s food in Mumbai and is a popular street food snack across whole Maharashtra. It is basically a savory slider of fried mashed potato fritters stuffed in a bread. ",
+    },
+    {
+      name: "Shawarma",
+      rating: "5⭐",
+      descrip:
+        "Shawarma is a dish in Middle Eastern cuisine consisting of meat cut into thin slices, stacked in a cone-like shape, and roasted on a slowly-turning vertical rotisserie or spit. ",
+    },
+    {
+      name: "Momos",
+      rating: "5⭐",
+      descrip:
+        "Momo is a type of East and South Asian steamed filled dumpling.",
+    },
+  ],
+};
